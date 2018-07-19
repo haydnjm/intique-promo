163addb0ff651e04ae3a3a6185443896fe3ae01e
unknown <haydnjmorris@googlemail.com>
2018-07-19 11:11:51 +0100

    success message alteration

--- client/src/reducers/signupStateReducer.js
+++ client/src/reducers/signupStateReducer.js
@@ -6,7 +6,8 @@ import {
 export default (state = '', action) => {
   switch (action.type) {
     case SIGNUP_SUCCESS:
-      return `Thank you for signing up to become a dealer!`;
+      return `Thanks for showing your interest! We'll be in contact over the
+              coming weeks to get you set up with your shiny new boutique!`;
     case SIGNUP_FAIL:
       return action.payload;
     default:
